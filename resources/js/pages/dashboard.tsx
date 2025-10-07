1f2f1cda637b93fd8a07d62f22bb46d787c62cfa
Arieloou <ariel.anchapaxi@gmail.com>
2025-10-06 20:52:32 -0500

    Git testing

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -2,7 +2,6 @@ import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
-
 import { Head } from '@inertiajs/react';
 
 const breadcrumbs: BreadcrumbItem[] = [
