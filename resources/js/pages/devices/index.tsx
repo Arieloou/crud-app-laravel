eb4cc0f3ac8c985990c389662a52aecafefcedf8
Arieloou <ariel.anchapaxi@gmail.com>
2025-10-07 20:24:06 -0500

    Upgrading the components styles of the app (Table, Buttons)

--- resources/js/pages/devices/index.tsx
+++ resources/js/pages/devices/index.tsx
@@ -3,6 +3,10 @@ import Pagination from "@/components/pagination";
 import { route }  from "ziggy-js";
 import { Head, Link, router } from "@inertiajs/react";
 import { PaginatedData, Device } from "@/types/global";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { IconPlus } from "@tabler/icons-react"
+import { Button } from "@/components/ui/button";
+import { ButtonGroup, ButtonGroupSeparator } from "@/components/ui/button-group";
 
 export default function IndexDevice({ devices, success } : { devices: PaginatedData<Device>; success? : string }) {
     const handleDelete = (id: number) => {
@@ -21,57 +25,68 @@ export default function IndexDevice({ devices, success } : { devices: PaginatedD
                 {success && <p className="bg-green-100 text-green-700 p-2 rounded mb-4">{success}</p>}
 
                 <div className="flex justify-end mb-4">
-                    <Link
-                        href={route("devices.create")}
-                        className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
-                    >
-                        + Nuevo Dispositivo
-                    </Link>
+                    <Button>
+                        <Link
+                            href={route("devices.create")}>
+                            + Nuevo Dispositivo
+                        </Link>
+                    </Button>
+
+                    <ButtonGroup>
+                        <Button variant="secondary">Button</Button>
+                        <ButtonGroupSeparator />
+                        <Button size="icon" variant="secondary">
+                            <IconPlus />
+                        </Button>
+                    </ButtonGroup>
                 </div>
 
-                <table className="w-full bg-white border rounded">
-                    <thead className="bg-gray-100">
-                        <tr>
-                            <th className="p-2 border">Serial</th>
-                            <th className="p-2 border">Categoría</th>
-                            <th className="p-2 border">Modelo</th>
-                            <th className="p-2 border">Estado</th>
-                            <th className="p-2 border">Fecha de Compra</th>
-                            <th className="p-2 border">Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[100px]">Serial</TableHead>
+                            <TableHead>Categoría</TableHead>
+                            <TableHead>Modelo</TableHead>
+                            <TableHead>Estado</TableHead>
+                            <TableHead>Fecha de Compra</TableHead>
+                            <TableHead className="text-right">Acciones</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
                         {devices.data.map((device) => (
-                            <tr key={device.id}>
-                                <td className="border p-2">{device.serial_number}</td>
-                                <td className="border p-2">{device.category?.name}</td>
-                                <td className="border p-2">{device.device_model?.name}</td>
-                                <td className="border p-2">{device.state}</td>
-                                <td className="border p-2">{new Date(device.purchase_date).toLocaleDateString()}</td>
-                                <td className="border p-2 flex gap-2">
-                                    <Link
-                                        href={route("devices.show", { id: device.id })}
-                                        className="text-blue-600 hover:underline"
-                                    >
-                                        Ver
-                                    </Link>
-                                    <Link
-                                        href={route("devices.edit", { id: device.id })}
-                                        className="text-yellow-600 hover:underline"
-                                    >
-                                        Editar
-                                    </Link>
-                                    <button
-                                        onClick={() => handleDelete(device.id)}
-                                        className="text-red-600 hover:underline"
-                                    >
-                                        Eliminar
-                                    </button>
-                                </td>
-                            </tr>
+                            <TableRow key={device.id}>
+                                <TableCell className="font-medium">{device.serial_number}</TableCell>
+                                <TableCell>{device.category?.name}</TableCell>
+                                <TableCell>{device.device_model?.name}</TableCell>
+                                <TableCell>{device.state}</TableCell>
+                                <TableCell>{new Date(device.purchase_date).toLocaleDateString()}</TableCell>
+                                <TableCell>
+                                    <div className="flex flex-row items-start gap-1">
+                                        <Button variant={'outline'}>
+                                            <Link
+                                                href={route("devices.show", { id: device.id })} 
+                                                className="text-black">
+                                                Ver
+                                            </Link>
+                                        </Button>
+                                        <Button>
+                                            <Link
+                                                href={route("devices.edit", { id: device.id })}
+                                                className="text-yellow-600 hover:underline">
+                                                Editar
+                                            </Link>
+                                        </Button>
+                                        <Button
+                                            onClick={() => handleDelete(device.id)}
+                                            className="text-red-600 hover:underline">
+                                            Eliminar
+                                        </Button>
+                                    </div>
+                                </TableCell>
+                            </TableRow>
                         ))}
-                    </tbody>
-                </table>
+                    </TableBody>
+                </Table>
 
                 <Pagination links={devices.links} />
             </div>
